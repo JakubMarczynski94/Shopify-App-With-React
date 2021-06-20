81be8cd2acc3b31dfae99a292f125232a56e37cb
philljohnson <philljohnson739@gmail.com>
2021-06-21 02:33:29 +0430

    create management layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import { Component } from 'react';
-
+import StoreHeader from './components/StoreHeader/StoreHeader.component'
+import MainLayout from './layout/mainLayout';
+import ListMenu from './components/ListMenu/ListMenu.component'
+import PanelHeader from './components/PanelHeader/PanelHeader.component'
+import ManagementLayout from './layout/ManagementLayout'
 
 class App extends Component{
   render(){
     return(
       <>
+      
+    {/* <MainLayout>
+      <ListMenu/>
+    </MainLayout> */}
+    {/* <PanelHeader>
+    <ListMenu/>
+
+    </PanelHeader> */}
+
+    <ManagementLayout>
+    <ListMenu/>
+
+    </ManagementLayout>
       </>
     )
   }
