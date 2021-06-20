7b9411a68de1650bb45bba48e53e7158444710d5
webmaster1022 <seniordev1019@gmail.com>
2021-06-20 17:24:10 +0430

    create FA-IR theme for MUI

---
+++ src/configs/RTLprovider.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import {faIR } from '@material-ui/core/locale';
+
+export const theme = createMuiTheme({
+  palette: {
+    // change theme color here :
+    primary: { main: '#1976d2' },
+  },
+}, faIR);
+
