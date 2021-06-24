9529842727f9575d929be1527f34df02247490d2
philljohnson <philljohnson739@gmail.com>
2021-06-24 20:09:51 +0430

    new API configuration to get any data any where

--- src/api/API.js
+++ src/api/API.js
@@ -1,9 +1,9 @@
 import API from './Config.api'
 
-export function getCustomersList(pageNumber, rowNumber) {
+export function getData(field,pageNumber, rowNumber) {
   // get example :
   //  http://localhost:3001/products?_page=2&_limit=3
-  return API.get(`/customers?_page=${pageNumber}&_limit=${rowNumber}`)
+  return API.get(`/${field}?_page=${pageNumber}&_limit=${rowNumber}`)
     .then(response => {
     
       console.log(response.headers['x-total-count'])
