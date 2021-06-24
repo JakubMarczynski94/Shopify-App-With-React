d04f9f56f4618bac758d6f2cbdf7f5670e1b9af0
webmaster1022 <seniordev1019@gmail.com>
2021-06-24 15:18:56 +0430

    Implement Router Structure

---
+++ src/router/Routes.js
@@ -0,0 +1,53 @@
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import createHistrory from "history/createBrowserHistory"
+import { MainLayout, ManagementLayout } from '../layout/index.layout'
+import { Quantity, Products, SignIn } from "../pages/index.pages";
+import {ListMenu} from '../components/ListMenu/ListMenu.component'
+import React from 'react'
+
+function Routes() {
+  const location = createHistrory()
+  return (
+    <BrowserRouter
+      history={location}
+    >
+
+      <Switch>
+        <Route path='/' exact>
+          <MainLayout >
+            <ListMenu/>
+          </MainLayout>
+        </Route>
+        <Route path='/panel/login' exact>
+          <ManagementLayout>
+            <SignIn />
+          </ManagementLayout>
+        </Route>
+        <Route path='/panel/login' exact>
+          <ManagementLayout>
+            <SignIn />
+          </ManagementLayout>
+        </Route>
+        <Route path='/panel/products' exact>
+          <ManagementLayout>
+            <Products />
+          </ManagementLayout>
+        </Route>
+        <Route path='/panel/quantity' exact>
+          <ManagementLayout>
+            <Quantity />
+          </ManagementLayout>
+        </Route>
+        <Route path='/panel' >
+          <ManagementLayout>
+            <SignIn />
+          </ManagementLayout>
+        </Route>
+
+
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export { Routes }
