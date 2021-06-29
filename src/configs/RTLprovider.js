56f7bde58b1a33b5236f322e5c1e5e4ac45c51b7
philljohnson <philljohnson739@gmail.com>
2021-06-29 17:12:12 +0430

    get theme new color & change css

--- src/configs/RTLprovider.js
+++ src/configs/RTLprovider.js
@@ -4,7 +4,7 @@ import {faIR } from '@material-ui/core/locale';
 export const theme = createMuiTheme({
   palette: {
     // change theme color here :
-    primary: { main: '#1976d2' },
+    primary: { main: '#3f51b5' },
   },
 }, faIR);
 
