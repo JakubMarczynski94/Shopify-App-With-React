f274d37045fd6925b52a2b5eb4397dd81ae2fafd
philljohnson <philljohnson739@gmail.com>
2021-07-03 22:59:31 +0430

    Access project to redux store

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {ThemeProvider} from '@material-ui/core/styles'
-import {theme} from './configs/RTLprovider'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { theme } from './configs/RTLprovider'
 import './asset/global.scss'
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
