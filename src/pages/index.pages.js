62dd637633ff45fbeae7fc90faebda12eb4c587d
philljohnson <philljohnson739@gmail.com>
2021-07-06 11:59:04 +0430

    Update indexes

--- src/pages/index.pages.js
+++ src/pages/index.pages.js
@@ -1,4 +1,10 @@
-export {Products} from './Management/Products/Products.page' 
-export {Quantity} from './Management/Quantity/Quantity.page'
-export {Orders} from './Management/Orders/Orders.page'
-export {SignIn} from './Management/LoginPanel/LoginPanel.page'
+export { Products } from './Management/Products/Products.page'
+export { Quantity } from './Management/Quantity/Quantity.page'
+export { Orders } from './Management/Orders/Orders.page'
+export { SignIn } from './Management/LoginPanel/LoginPanel.page'
+
+export {MainCustomers } from './Customers/MainCustomers/MainCustomers.page'
+export {ProductsList} from './Customers/ProductsList/ProducsList.page'
+export {ProductDetails} from './Customers/ProductDetails/ProductDetails.page'
+export {Payment} from './Customers/Payment/Payment.page'
+
