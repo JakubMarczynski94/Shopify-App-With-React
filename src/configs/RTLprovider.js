cc533bdd1aed5d8895db8ab5e373ed14d3c567ea
webmaster1022 <seniordev1019@gmail.com>
2021-07-06 12:03:25 +0430

    Add Yekan font to typography in general theme

--- src/configs/RTLprovider.js
+++ src/configs/RTLprovider.js
@@ -1,10 +1,36 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import {faIR } from '@material-ui/core/locale';
+import { faIR } from '@material-ui/core/locale';
+import Yekan from '../asset/font/Yekan.ttf'
 
 export const theme = createMuiTheme({
+  typography: {
+    fontFamily: Yekan,
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [Yekan],
+      },
+    },
+  },
   palette: {
     // change theme color here :
     primary: { main: '#3f51b5' },
+
   },
 }, faIR);
 
+
+
+export const fontTheme = createMuiTheme({
+  typography: {
+    fontFamily: Yekan,
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [Yekan],
+      },
+    },
+  },
+});
