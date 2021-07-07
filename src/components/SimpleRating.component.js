f7a62504db03af24a2deba7e0dbd3428a10aef0a
philljohnson <philljohnson739@gmail.com>
2021-07-07 11:55:14 +0430

    create rate star

---
+++ src/components/SimpleRating.component.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
+
+
+export function SimpleRating(props) {
+  const [value, setValue] = React.useState(2);
+  const randomRate = Math.floor(Math.random() * 5) + 1
+
+
+  return (
+    <div dir='ltr'>
+      <Box component="fieldset" mt={1} mb={2} borderColor="transparent"  >
+        {/* <Typography component="legend">امتیاز کالا</Typography> */}
+        <Rating
+          size='small'
+          name="simple-controlled"
+          value={randomRate}
+          readOnly
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+      </Box>
+
+    </div>
+  );
+}
