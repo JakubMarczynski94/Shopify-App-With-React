bd96bb89e07a94718837bf84c6deec4645db8f6c
philljohnson <philljohnson739@gmail.com>
2021-07-07 11:52:40 +0430

    add action for quantity change

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,15 @@
-import { SET_NEW_QUANTITY } from "./types"
+import { DELETE_QUANTITY_CHANGE_LOG, SET_NEW_QUANTITY } from "./types"
 
 export const setNewQuantity = (payload) => {
   return {
     type: SET_NEW_QUANTITY,
     payload: payload
+  }
+}
+
+export const deleteQuantityChangeLog = () => {
+  return {
+    type: DELETE_QUANTITY_CHANGE_LOG
+
   }
 }
