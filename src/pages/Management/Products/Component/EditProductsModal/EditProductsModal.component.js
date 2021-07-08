a07f8f1856d211b510a70f93f8bb28d4ecbfd39d
webmaster1022 <seniordev1019@gmail.com>
2021-07-08 22:49:18 +0430

    edit select group

--- src/pages/Management/Products/Component/EditProductsModal/EditProductsModal.component.js
+++ src/pages/Management/Products/Component/EditProductsModal/EditProductsModal.component.js
@@ -17,6 +17,8 @@ import { UploadFile } from '../UploadFile/UploadFile.component'
 
 import * as AXIOS from '../../../../../api/API'
 
+import { toast } from 'react-toastify';
+
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -43,16 +45,52 @@ const useStyles = makeStyles((theme) => ({
 export function EditProductsModal(props) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
-  const [state, setState] = React.useState({ image: '', name: '', branch: '', information: '' })
+  const [state, setState] = React.useState({ image: '', name: '', group: '', information: '' })
 
-  const handlePostData = async () => {
-    let bodyFormData = new FormData()
-    bodyFormData.append('name', state.name)
-    bodyFormData.append('information', state.information)
-    bodyFormData.append('branch', state.branch)
-    bodyFormData.append('image', state.image)
+  const handlePatchData = async () => {
+    let data = new FormData()
+    data.append('name', state.name)
+    data.append('information', state.information)
+    data.append('group', state.group)
+    data.append('image', state.image)
 
-    await AXIOS.postProducts(bodyFormData)
+    // var params = new URLSearchParams();
+    // state.name && params.append('name', state.name);
+    // state.information && params.append('information', state.information);
+    // state.branch && params.append('group', state.branch)
+
+
+    const group = props.group
+    const id = props.productId
+
+    try {
+      await AXIOS.editProducts(data, group, id)
+      handleClose()
+      toast.success(<p dir='rtl'> &emsp;<strong> ✔ </strong> &ensp;ویرایش با موفقیت انجام شد    </p>, {
+        position: "bottom-left",
+        autoClose: 7000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+
+    }
+    catch (error) {
+      console.log(error.message)
+      toast.error(<p>{error.message}</p>, {
+        position: "bottom-left",
+        autoClose: 7000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+
+    }
+
 
   }
 
@@ -70,7 +108,7 @@ export function EditProductsModal(props) {
       <span onClick={handleOpen} className={classes.button} >
         ویرایش کالا
       </span>
-      <Modal style={{overflow:'scroll'}}
+      <Modal style={{ overflow: 'scroll' }}
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         className={classes.modal}
@@ -92,19 +130,19 @@ export function EditProductsModal(props) {
             </div>
 
             <p>تصویر کالا</p>
-            <UploadFile image={(image) => setState({ ...state, image })}  defaultValue={props.imageName} />
+            <UploadFile image={(image) => setState({ ...state, image })} defaultValue={props.imageName} />
 
             <p>نام کالا</p>
             <BasicTextFields value={(value) => setState({ ...state, name: value })} defaultValue={props.name} />
 
             <p> دسته بندی</p>
-            <SimpleSelect value={(value) => setState({ ...state, branch: value })} />
+            <SimpleSelect group={props.group} value={(value) => setState({ ...state, group: value })} />
 
             <p>  توضیحات </p>
-            <TextArea value={(value) => setState({ ...state, information: value })} />
+            <TextArea defaultValue={props.information} value={(value) => setState({ ...state, information: value })} />
 
             <div style={{ display: 'flex', justifyContent: 'center' }} >
-              <Button onClick={handlePostData} variant="contained" color="primary" style={{ width: '100px', marginTop: '10px' }}>
+              <Button onClick={handlePatchData} variant="contained" color="primary" style={{ width: '100px', marginTop: '10px' }}>
                 ذخیره
               </Button>
             </div>
@@ -181,16 +219,17 @@ function SimpleSelect(props) {
         <Select
           labelId="demo-simple-select-outlined-label"
           id="demo-simple-select-outlined"
-          value={age}
+          value={props.group}
           onChange={handleChange}
         // label="Age"
         >
-           <MenuItem value="">
+          <MenuItem value="">
             <em>گروه محصول</em>
           </MenuItem>
-          <MenuItem value={10}>کالاهای اساسی و خوار و بار</MenuItem>
-          <MenuItem value={20}>کالاهای اساسی و خوار و بار</MenuItem>
-          <MenuItem value={30}>کالاهای اساسی و خوار و بار</MenuItem>
+          <MenuItem value={'groceries'}>کالاهای اساسی و خوار و بار</MenuItem>
+          <MenuItem value={'dairies'}>لبنیات</MenuItem>
+          <MenuItem value={'proteins'}>محصولات پروتئینی</MenuItem>
+          <MenuItem value={'drinkd'}>نوشیدنی و</MenuItem>
         </Select>
       </FormControl>
 
@@ -203,10 +242,13 @@ function TextArea(props) {
   const handleChange = (event) => {
     const value = event.target.value
     props.value(value)
+
   }
 
   return (
-    <textarea className={style.textArea} onChange={handleChange} />
+    <textarea className={style.textArea} onChange={handleChange}  >
+      {props.defaultValue}
+    </textarea>
   )
 }
 
