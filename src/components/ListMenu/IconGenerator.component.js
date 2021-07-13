892fc994c1cf70edf56c8732b17cc744e92c8bf9
webmaster1022 <seniordev1019@gmail.com>
2021-07-13 13:39:59 +0430

    Add icon generator component

---
+++ src/components/ListMenu/IconGenerator.component.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function IconGenerator(props) {
+  const style = {
+    icon: {
+      width: 20,
+      height: 20,
+      marginLeft:5
+    }
+  }
+  return (
+
+    <i style={style.icon} className={`${props.fa_iconName}`} ></i>
+
+  )
+}
