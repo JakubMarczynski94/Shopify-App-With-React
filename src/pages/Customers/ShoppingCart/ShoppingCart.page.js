24bf3568c46e325956560f4cb974435215d90e2e
webmaster1022 <seniordev1019@gmail.com>
2021-07-13 13:46:09 +0430

    Update ShoppingCart.page.js

--- src/pages/Customers/ShoppingCart/ShoppingCart.page.js
+++ src/pages/Customers/ShoppingCart/ShoppingCart.page.js
@@ -1,20 +1,12 @@
 import React, { useEffect, useState } from 'react';
-// import Modal from '@material-ui/core/Modal';
-// import Backdrop from '@material-ui/core/Backdrop';
-// import Fade from '@material-ui/core/Fade';
-// // import style from './OrdersModal.module.scss'
-// import CancelIcon from '@material-ui/icons/Cancel';
+
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-// import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-// import { Button } from '@material-ui/core';
-// import { editProducts as editOrder } from '../../../../../api/API';
-// import { toast } from 'react-toastify';
 import { connect } from 'react-redux';
 import { deleteCart } from '../../../redux/actions';
 import { Toolbar } from '@material-ui/core';
@@ -59,7 +51,7 @@ const useStyles = makeStyles({
     padding: 7
   },
   button: {
-    height:40
+    height: 40
   },
   container: {
     display: 'flex',
@@ -72,15 +64,15 @@ const useStyles = makeStyles({
     textAlign: 'center',
     alignSelf: 'center'
   },
-  tableBottom:{
-    display:'flex',
-    justifyContent:'space-between',
-    padding:0,
-    alignItems:'center'
+  tableBottom: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: 0,
+    alignItems: 'center'
 
   },
-  link:{
-    textDecoration:'none'
+  link: {
+    textDecoration: 'none'
   }
 
 });
@@ -112,41 +104,6 @@ function Cart(props) {
 
 
 
-
-  const orderDelivered = async () => {
-    //   try {
-    //     const id = props.data.id
-    //     const group = 'orders'
-    //     const data = new FormData()
-    //     const endProccessTime = Date.now()
-    //     data.append('delivered', 'true')
-    //     data.append('endProccessTime', endProccessTime)
-    //     await editOrder(data, group, id)
-    //     props.handleClose(true)
-    //     toast.success(<p dir='rtl'> &emsp;<strong> ✔ </strong> &ensp;تغییر وضعیت به :<strong>  تحویل شد   </strong>   </p>, {
-    //       position: "bottom-left",
-    //       autoClose: 7000,
-    //       hideProgressBar: false,
-    //       closeOnClick: true,
-    //       pauseOnHover: true,
-    //       draggable: true,
-    //       progress: undefined,
-    //     });
-    //   }
-    //   catch (error) {
-    //     toast.error(<p>{error.message}</p>, {
-    //       position: "bottom-left",
-    //       autoClose: 7000,
-    //       hideProgressBar: false,
-    //       closeOnClick: true,
-    //       pauseOnHover: true,
-    //       draggable: true,
-    //       progress: undefined,
-    //     });
-    //   }
-
-  }
-
   return (
     <TableContainer component={Paper} className={classes.paper}>
       <Toolbar />
@@ -163,41 +120,41 @@ function Cart(props) {
 
               </TableRow>
             </TableHead>
-          {
-            state.data[0] ? <TableBody>
             {
-              state.data.map((row) => (
-                <StyledTableRow key={row.id}>
+              state.data[0] ? <TableBody>
+                {
+                  state.data.map((row) => (
+                    <StyledTableRow key={row.id}>
 
-                  <StyledTableCell className={classes.row} align="right">
-                  <Link className={classes.link} to={`/home/products/${row.group}/${row.subgroup }/${row.productId}`}>
-                    {row.productName}
-                    </Link>
-                  </StyledTableCell>
-                  <StyledTableCell className={classes.row} align="right">{row.productPrice}</StyledTableCell>
-                  <StyledTableCell className={classes.row} align="right">{row.number}</StyledTableCell>
+                      <StyledTableCell className={classes.row} align="right">
+                        <Link className={classes.link} to={`/home/products/${row.group}/${row.subgroup}/${row.productId}`}>
+                          {row.productName}
+                        </Link>
+                      </StyledTableCell>
+                      <StyledTableCell className={classes.row} align="right">{row.productPrice}</StyledTableCell>
+                      <StyledTableCell className={classes.row} align="right">{row.number}</StyledTableCell>
 
-                </StyledTableRow>)
-              )
+                    </StyledTableRow>)
+                  )
+                }
+              </TableBody> :
+                <p className={classes.noCartText} > محصولی در سبد خرید موجود نمی باشد </p>
             }
-          </TableBody> :
-          <p className={classes.noCartText} > محصولی در سبد خرید موجود نمی باشد </p>
-          }
-           
+
           </Table>
         </div>
 
         <div className={classes.tableBottom} >
           <p>جمع خرید : </p>
           {
-            state.data[0] ?        <Link className={classes.link} to='/checkout'>
-            <Button   variant='contained' color='secondary' className={classes.button} >
-              نهایی کردن سبد خرید
-            </Button>
+            state.data[0] ? <Link className={classes.link} to='/checkout'>
+              <Button variant='contained' color='secondary' className={classes.button} >
+                نهایی کردن سبد خرید
+              </Button>
             </Link> :
-            <Button disabled='true'  variant='contained' color='secondary' className={classes.button} >
-            نهایی کردن سبد خرید
-          </Button>
+              <Button disabled='true' variant='contained' color='secondary' className={classes.button} >
+                نهایی کردن سبد خرید
+              </Button>
           }
         </div>
 
