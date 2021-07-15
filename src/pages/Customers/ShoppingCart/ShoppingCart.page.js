02ff0155cd35c295d56c46e8f9904ed48bffb83b
webmaster1022 <seniordev1019@gmail.com>
2021-07-15 17:39:36 +0430

    fix delete order and rerender after that

--- src/pages/Customers/ShoppingCart/ShoppingCart.page.js
+++ src/pages/Customers/ShoppingCart/ShoppingCart.page.js
@@ -8,12 +8,16 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { connect } from 'react-redux';
-import { deleteCart } from '../../../redux/actions';
+import { addSumPrice, deleteCart } from '../../../redux/actions';
 import { Toolbar } from '@material-ui/core';
 import { TableContainer } from '../../../components/index.components'
 import { Paginate } from '../../../components/index.components'
 import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import DeleteOrder from './DeleteOrder.component'
+import { toEnglishDigit, toPersinaDigit } from '../../../utils/convertNumber';
+import { wordToPersian } from '../../../utils/convertNameToPersian';
+import { BASE_URL } from '../../../api/Variables.api';
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -68,11 +72,34 @@ const useStyles = makeStyles({
     display: 'flex',
     justifyContent: 'space-between',
     padding: 0,
-    alignItems: 'center'
+    alignItems: 'center',
+
 
   },
   link: {
     textDecoration: 'none'
+  },
+  sumPriceText: {
+    color: '#111',
+    fontWeight: 'bold',
+    // borderBottom:'3px solid #ccc',
+    padding: '7px 10px',
+    margin: 0,
+    boxShadow: '0 0px 3px #ccc',
+    borderRadius: '4px',
+    height:38,
+    backgroundColor: 'rgb(206, 255, 206)'
+  },
+  image:{
+    width:40,
+    height:40,
+    borderRadius:5,
+    border:'.5px solid #ccc'
+
+  },
+  imageCell:{
+    margin:0,
+    padding:'6px 4px 0px 4px',
   }
 
 });
@@ -87,12 +114,30 @@ const useStyles = makeStyles({
 function Cart(props) {
 
   const [state, setState] = useState({ data: [{}] })
-  useEffect(() => {
-    const cart = props.cart
-    setState({ data: cart })
-    console.log(cart)
+  const [isRerender, setIsRerender] = useState('true')
+  const [sumPrice, setSumPrice] = useState(0)
 
-  }, [])
+
+  useEffect(async () => {
+    if (isRerender === 'true') {
+
+      // console.log(props.cart, 'props???')
+      setIsRerender('false')
+      const cart = props.cart
+      await setState({ data: cart })
+      sumCart()
+
+
+    }
+    else {
+      const cart = props.cart
+      await setState({ data: cart })
+      sumCart()
+    }
+  }, [isRerender])
+
+  // useEffect(()=>{
+  // },[state])
 
   const classes = useStyles();
 
@@ -100,6 +145,27 @@ function Cart(props) {
     const date = new Date(+epoch)
     const persianDate = date.toLocaleString('fa-IR')
     return persianDate
+  }
+
+  const handleRerender = async (isRerender) => {
+    console.log('rerender called')
+    await setIsRerender(isRerender)
+  }
+
+  const sumCart = () => {
+    let sumPrice = 0
+    state.data.map(item => {
+      const price = toEnglishDigit(item.productPrice)
+      const count = item.number
+
+      const sum = count * price
+
+      sumPrice += sum
+    })
+    const persianNumberPrice = new Number(sumPrice).toLocaleString("fa-IR")
+    setSumPrice(persianNumberPrice)
+    props.addSumPrice(persianNumberPrice)
+
   }
 
 
@@ -113,9 +179,11 @@ function Cart(props) {
             <TableHead>
               <TableRow>
 
+                <StyledTableCell align="right">تصویر </StyledTableCell>
                 <StyledTableCell align="right">کالا </StyledTableCell>
                 <StyledTableCell align="right"> قیمت</StyledTableCell>
                 <StyledTableCell align="right">  تعداد </StyledTableCell>
+                <StyledTableCell align="right">  </StyledTableCell>
 
 
               </TableRow>
@@ -126,6 +194,8 @@ function Cart(props) {
                   state.data.map((row) => (
                     <StyledTableRow key={row.id}>
 
+                      <StyledTableCell className={classes.imageCell} align="right"><img className={classes.image}  src={`${BASE_URL}${row.image}`} alt='تصویر کالا' />   </StyledTableCell>
+
                       <StyledTableCell className={classes.row} align="right">
                         <Link className={classes.link} to={`/home/products/${row.group}/${row.subgroup}/${row.productId}`}>
                           {row.productName}
@@ -133,6 +203,9 @@ function Cart(props) {
                       </StyledTableCell>
                       <StyledTableCell className={classes.row} align="right">{row.productPrice}</StyledTableCell>
                       <StyledTableCell className={classes.row} align="right">{row.number}</StyledTableCell>
+                      <StyledTableCell className={classes.row} align="right">
+                        <DeleteOrder id={row.id} group={row.group} isRerender={handleRerender} newCart={(newCart => setState(newCart))} />
+                      </StyledTableCell>
 
                     </StyledTableRow>)
                   )
@@ -145,7 +218,7 @@ function Cart(props) {
         </div>
 
         <div className={classes.tableBottom} >
-          <p>جمع خرید : </p>
+          <p className={classes.sumPriceText} >جمع خرید : {sumPrice}  تومان </p>
           {
             state.data[0] ? <Link className={classes.link} to='/checkout'>
               <Button variant='contained' color='secondary' className={classes.button} >
@@ -170,7 +243,10 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => {
-  return { deleteCart: () => dispatch(deleteCart()) }
+  return {
+    deleteCart: () => dispatch(deleteCart()),
+    addSumPrice: (price) => dispatch(addSumPrice(price))
+  }
 }
 
 
