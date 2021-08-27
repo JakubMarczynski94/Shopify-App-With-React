be2ce8210fb2bf65a52374b8b2fea849ff189e8f
webmaster1022 <seniordev1019@gmail.com>
2021-08-27 12:10:41 +0430

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,11 @@
-import { ADD_PRODUCT_TO_CART, ADD_SUM_PRICE, DELETE_CART, DELETE_ORDER, DELETE_QUANTITY_CHANGE_LOG, EDIT_CART, SET_NEW_QUANTITY } from "./types"
+import { ADD_PRODUCT_TO_CART,
+        ADD_SUM_PRICE,
+        DELETE_CART,
+        DELETE_ORDER,
+        DELETE_QUANTITY_CHANGE_LOG,
+        EDIT_CART,
+        SET_NEW_QUANTITY } from "./types"
+
 
 export const setNewQuantity = (payload) => {
   return {
